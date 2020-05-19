5ef46bf7f936d87168a93f855ce8103fb12f21ee
Paul van Leeuwen <paul@vk4omen-ubuntu>
2020-05-19 14:12:18 +0200

    cleanup

--- challenges/programming/irc_the_romans_wheel.rb
+++ challenges/programming/irc_the_romans_wheel.rb
@@ -5,6 +5,6 @@ require 'challenges/programming/irc'
 irc = IRC.new
 irc.send('PRIVMSG Candy :!ep3')
 match = irc.expect(/PRIVMSG #{ARGV[0]} :(?<enc>\w+)/)
-result = match[:enc].tr("A-Za-z", "N-ZA-Mn-za-m")
+result = match[:enc].tr('A-Za-z', 'N-ZA-Mn-za-m')
 irc.send("PRIVMSG Candy :!ep3 -rep #{result}")
 irc.expect(/You dit it!/)
